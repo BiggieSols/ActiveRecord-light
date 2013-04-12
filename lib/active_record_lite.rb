e03749f6cace7292b27c2624894a4ef72f233dc7
Ned Ruggeri <ruggeri@self-loop.com>
2013-04-12 09:10:13 -0700

    remove bulk of code

--- lib/active_record_lite.rb
+++ lib/active_record_lite.rb
@@ -1,5 +1,7 @@
-require_relative './active_record_lite/associatable'
+# NR: Comment these back in as you write them.
+
+#require_relative './active_record_lite/associatable'
 require_relative './active_record_lite/db_connection'
-require_relative './active_record_lite/mass_object'
-require_relative './active_record_lite/searchable'
-require_relative './active_record_lite/sql_object'
+#require_relative './active_record_lite/mass_object'
+#require_relative './active_record_lite/searchable'
+#require_relative './active_record_lite/sql_object'
