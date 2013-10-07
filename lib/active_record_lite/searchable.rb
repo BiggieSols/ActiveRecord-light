56de20703c32a6a2a5a374ce624864985ca5046a
Asher King Abramson <a.kingabramson@gmail.com>
2013-10-07 11:26:18 -0700

    fixed filenames and changed set_attrs to my_attr_accessible

---
+++ lib/active_record_lite/searchable.rb
@@ -0,0 +1,15 @@
+require_relative './db_connection'
+
+module Searchable
+  def where(params)
+    where_line = params.map { |key, val| "#{key} = ?" }.join(" AND ")
+
+    results = DBConnection.execute(<<-SQL, *params.values)
+      SELECT *
+        FROM #{table_name}
+       WHERE #{where_line}
+    SQL
+
+    parse_all(results)
+  end
+end
