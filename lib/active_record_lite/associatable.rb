95d551a89ea1fcfad3d3d4784220dd3531b933e8
enquea <student@appacademy.io>
2013-12-07 18:01:54 -0800

    associatable complete

--- lib/active_record_lite/associatable.rb
+++ lib/active_record_lite/associatable.rb
@@ -24,6 +24,7 @@ class BelongsToAssocParams < AssocParams
   end
 
   def type
+    :belongs_to
   end
 end
 
@@ -32,69 +33,76 @@ class HasManyAssocParams < AssocParams
     @other_class_name = params[:other_class_name] || name.to_s.singularize.camelize
     @foreign_key      = params[:foreign_key]      || self_class.foreign_key
     @primary_key      = params[:primary_key]      || :id
-
-    puts "other class is #{@other_class_name}"
-    puts "foreign  key is #{@foreign_key}"
-    puts "primary key is #{@primary_key}"
-    puts "other table is #{other_table}"
   end
 
   def type
+    :has_many
   end
 end
-
-# dog --> owner
 
 
 module Associatable
   def assoc_params
+    @association_params ||= {}
+    @association_params
   end
 
   def belongs_to(name, params = {})
-    assoc_params = BelongsToAssocParams.new(name, params)
+    asn_params = BelongsToAssocParams.new(name, params)
+    assoc_params[name] = asn_params
 
     define_method(name) do
-      results = DBConnection.execute(<<-SQL, self.send(assoc_params.foreign_key))
+      results = DBConnection.execute(<<-SQL, self.send(asn_params.foreign_key))
         SELECT
           *
         FROM
-          #{assoc_params.other_table}
+          #{asn_params.other_table}
         WHERE
-          #{assoc_params.other_table}.#{assoc_params.primary_key} = ?
+          #{asn_params.other_table}.#{asn_params.primary_key} = ?
       SQL
-      assoc_params.other_class.parse_all(results).first
+      # debugger
+      asn_params.other_class.parse_all(results).first
     end
   end
 
   def has_many(name, params = {})
-    assoc_params = HasManyAssocParams.new(name, params, self.class.to_s)
+    asn_params = HasManyAssocParams.new(name, params, self.class.to_s)
+    assoc_params[name] = asn_params
 
     define_method(name) do
-      results = DBConnection.execute(<<-SQL, self.send(assoc_params.primary_key))
+      results = DBConnection.execute(<<-SQL, self.send(asn_params.primary_key))
         SELECT
           *
         FROM
-          #{assoc_params.other_table}
+          #{asn_params.other_table}
         WHERE
-          #{assoc_params.other_table}.#{assoc_params.foreign_key} = ?
+          #{asn_params.other_table}.#{asn_params.foreign_key} = ?
       SQL
-
-    assoc_params.other_class.parse_all(results)
+    asn_params.other_class.parse_all(results)
     end
   end
 
   def has_one_through(name, assoc1, assoc2)
+
+
+    define_method(name) do
+      assoc_1 = self.class.assoc_params[assoc1]
+      assoc_2 = assoc_1.other_class.assoc_params[assoc2]
+
+      results = DBConnection.execute(<<-SQL, self.send(assoc_1.foreign_key))
+        SELECT
+          #{assoc_2.other_table}.*
+        FROM
+          #{assoc_1.other_table}
+        JOIN
+          #{assoc_2.other_table}
+        ON
+          #{assoc_2.other_table}.#{assoc_2.primary_key} =
+          #{assoc_1.other_table}.#{assoc_2.primary_key}
+        WHERE
+          #{assoc_1.other_table}.#{assoc_1.primary_key} = ?
+      SQL
+      assoc_2.other_class.parse_all(results).first
+    end
   end
 end
-
-# b = BelongsToAssocParams.new("sql_object")
-
-# b.instance_variables.each do |var|
-#   puts b.send(var)
-# end
-#
-
-# p b.other_class_name
-# p b.foreign_key
-# p b.primary_key
-# p b.other_table
